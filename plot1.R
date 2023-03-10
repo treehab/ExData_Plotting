85986565d920141a005ef54517f25e1cbf5219e1
treehab <88785923+treehab@users.noreply.github.com>
2023-03-11 01:22:53 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
 library("data.table")
 
-setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project/data")
+setwd("~/C:/Users/Aryaman/OneDrive/Documents/GIT/R programing/household_power_consumption.txt")
 
 #Reads in data from file then subsets data for specified dates
 powerDT <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
